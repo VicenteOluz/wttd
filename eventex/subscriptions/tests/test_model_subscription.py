9ce15917abab2cc9ac10c2ada2357af30c930e72
vicente.luz <vicenteoluz@gmail.com>
2017-06-21 15:27:01 -0300

    Persiste a Inscriçao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name="Vicente Luz",
+            cpf='12345678911',
+            email='vicente@frigotil.com.br',
+            phone='86-988221812'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,8 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+
 
 class SubscribeGet(TestCase):
     def setUp(self):
@@ -35,6 +37,7 @@ class SubscribeGet(TestCase):
         form = self.resp.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
+
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Vicente Luz', cpf='12345678911',
@@ -47,6 +50,9 @@ class SubscribePostValid(TestCase):
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
+
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 
 class SubscribePostInvalid(TestCase):
@@ -67,6 +73,10 @@ class SubscribePostInvalid(TestCase):
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
+
 
 class SubscribeSuccessMessage(TestCase):
     def test_message(self):
